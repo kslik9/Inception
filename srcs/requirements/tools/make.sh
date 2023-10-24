2c28bd7a67c7d389e610028e7000f30db35660c3
kslik9 <khalid.slik@usmba.ac.ma>
2023-10-24 14:59:35 +0000

    finish

--- srcs/requirements/tools/make.sh
+++ srcs/requirements/tools/make.sh
@@ -2,9 +2,9 @@
 
 RED='\033[0;31m'
 NC='\033[0m'
-data_dir="/home/kslik/data"
+data_dir="/home/${USER}/data"
 
-
+echo ${data_dir}
 if [ ! -d "$data_dir/mysql" ]; then
     echo "${RED}mysql volume directory does not exist, creating...${NC}"
     mkdir "$data_dir/mysql"
